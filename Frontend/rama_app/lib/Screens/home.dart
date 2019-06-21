34d8a182912145d124528888764cff832fa824c1
dakshpokar <dakshpokar@gmail.com>
2019-06-21 22:35:55 +0530

    Added SilverAppBar

--- Frontend/rama_app/lib/Screens/home.dart
+++ Frontend/rama_app/lib/Screens/home.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage>
   StreamSubscription<Event> _onModelAddedSubscription;
   StreamSubscription<Event> _onModelChangedSubscription;
   AnimationController _controller;
-
+  double pad = 0;
   DatabaseReference modelsRef;
 
   @override
@@ -142,7 +142,6 @@ class _HomePageState extends State<HomePage>
     } else {
       //_onModelChangedSubscription = modelsRef.onChildChanged.listen(_onModelUpdate);
       return Scaffold(
-          resizeToAvoidBottomPadding: true,
           backgroundColor: Theme.of(context).splashColor,
           bottomNavigationBar: FABBottomAppBar(
             onTabSelected: _selectedTab,
@@ -165,156 +164,156 @@ class _HomePageState extends State<HomePage>
             icon: Icon(Icons.book),
             elevation: 4.0,
             heroTag: "newModel",
+            hoverElevation: 10,
           ),
-          body: Container(
-              margin: EdgeInsets.only(top: 40, left: 10, right: 10),
-              color: Theme.of(context).splashColor,
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 60,
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      elevation: 10,
-                      child: InkWell(
-                        child: Container(
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: getRow(),
-                          ),
-                        ),
-                        onTap: search,
-                        splashColor: Colors.grey,
-                      ),
-                    ),
+          body: CustomScrollView(
+
+            slivers: <Widget>[
+              SliverPadding(
+                padding: EdgeInsets.only(top: 15),
+              ),
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                title: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Row(
+                    children: getRow(),
                   ),
 
-                  Container(
-                      child: Flexible(
-                    child: FirebaseAnimatedList(
-                      scrollDirection: Axis.vertical,
-                      query: modelsRef,
-                      itemBuilder: (BuildContext bc, DataSnapshot ds,
-                          Animation<double> animation, int index) {
-                        return Dismissible(
-                            key: Key(
-                                index.toString() + models.length.toString()),
-                            background: Container(
-                                padding: EdgeInsets.only(left: 20, right: 20),
-                                color: Theme.of(context).splashColor,
-                                child: Center(
-                                    child: Icon(
+                  margin: EdgeInsets.only(left: 5, right: 5),
+                  decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0,
+                    ),],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+                floating: true,
+                expandedHeight: 80,
+              ),
+
+              SliverList(
+
+                delegate: SliverChildBuilderDelegate((BuildContext bc, int index){
+
+                  return Container(
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    child: Dismissible(
+                        key: Key(
+                            index.toString() + models.length.toString()),
+                        background: Container(
+                            padding: EdgeInsets.only(left: 20, right: 20),
+                            color: Theme.of(context).splashColor,
+                            child: Center(
+                                child: Icon(
                                   Icons.delete,
                                   color: Colors.white,
                                 ))),
-                            confirmDismiss: _confirmDismiss,
-                            onDismissed: (direction) {
-                              if (models.contains(models[index])) {
-                                setState(() {
-                                  widget._db
-                                      .child(widget._id)
-                                      .child("models")
-                                      .child(models[index].id)
-                                      .remove();
-                                });
-                                models.removeAt(index);
-                              }
-                            },
-                            child: Stack(
-
-                              children: <Widget>[
-                                Container(
-                                  height: 124,
-                                  margin: EdgeInsets.only(left: 46, bottom: 10),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      shape: BoxShape.rectangle,
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      boxShadow: <BoxShadow>[
-                                        BoxShadow(
-                                            color: Colors.black12,
-                                            blurRadius: 10.0,
-                                            offset: Offset(0.0, 10.0))
-                                      ]),
-                                  child: Container(
-                                    alignment: Alignment.topLeft,
-                                    margin: EdgeInsets.only(left: 70, top: 15),
-                                    width: MediaQuery.of(context).size.width,
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                            models[index].name,
-                                          style: TextStyle(
-                                            fontSize: 25,
-                                            fontFamily: "Raleway"
-                                          ),
-                                        ),
-                                        Text(
-                                          models[index].id,
-                                          style: TextStyle(
-                                              fontSize: 15,
-                                              fontFamily: "Raleway"
-                                          ),
-                                        ),
-                                        Text(
-                                          models[index].lastUpdatedOn,
-                                          style: TextStyle(
-                                              fontSize: 15,
-                                              fontFamily: "Raleway"
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Container(
-
-                                  margin:
-                                      new EdgeInsets.symmetric(vertical: 16.0),
-                                  alignment: FractionalOffset.centerLeft,
-
-                                  child: ClipOval(
-                                    child:  WaveWidget(
-
-                                      config: CustomConfig(
-                                        gradients: [
-                                          [Colors.red, models[index].getColor(1)],
-                                          [Colors.blue, models[index].getColor(2)],
-                                          [Colors.green, models[index].getColor(3)],
-                                          [Colors.yellow, models[index].getColor(4)]
-                                        ],
-                                        durations: [35000, 19440, 10800, 6000],
-                                        heightPercentages: [0.20, 0.23, 0.25, 0.30],
-                                        blur: MaskFilter.blur(BlurStyle.solid, 10),
-                                        gradientBegin: Alignment.bottomLeft,
-                                        gradientEnd: Alignment.topRight,
+                        confirmDismiss: _confirmDismiss,
+                        onDismissed: (direction) {
+                          if (models.contains(models[index])) {
+                            setState(() {
+                              widget._db
+                                  .child(widget._id)
+                                  .child("models")
+                                  .child(models[index].id)
+                                  .remove();
+                            });
+                            models.removeAt(index);
+                          }
+                        },
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              height: 124,
+                              margin: EdgeInsets.only(left: 46, bottom: 10+pad),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  boxShadow: <BoxShadow>[
+                                    BoxShadow(
+                                        color: Colors.black12,
+                                        blurRadius: 10.0,
+                                        offset: Offset(0.0, 10.0))
+                                  ]),
+                              child: Container(
+                                alignment: Alignment.topLeft,
+                                margin: EdgeInsets.only(left: 70, top: 15),
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      models[index].name,
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontFamily: "Raleway"
                                       ),
-                                      waveAmplitude: 0,
-                                      backgroundColor: Colors.blue,
-                                      size: Size(90, 90),
-
                                     ),
-                                  ),
+                                    Text(
+                                      models[index].id,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontFamily: "Raleway"
+                                      ),
+                                    ),
+                                    Text(
+                                      models[index].lastUpdatedOn,
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontFamily: "Raleway"
+                                      ),
+                                    ),
+                                  ],
                                 ),
+                              ),
+                            ),
+                            Container(
 
-                              ],
-                            )
-                        );
-                      },
+                              margin:
+                              new EdgeInsets.symmetric(vertical: 16.0),
+                              alignment: FractionalOffset.centerLeft,
+
+                              child: ClipOval(
+                                child:  WaveWidget(
+
+                                  config: CustomConfig(
+                                    gradients: [
+                                      [Colors.red, models[index].getColor(1)],
+                                      [Colors.blue, models[index].getColor(2)],
+                                      [Colors.green, models[index].getColor(3)],
+                                      [Colors.yellow, models[index].getColor(4)]
+                                    ],
+                                    durations: [35000, 19440, 10800, 6000],
+                                    heightPercentages: [0.20, 0.23, 0.25, 0.30],
+                                    blur: MaskFilter.blur(BlurStyle.solid, 10),
+                                    gradientBegin: Alignment.bottomLeft,
+                                    gradientEnd: Alignment.topRight,
+                                  ),
+                                  waveAmplitude: 0,
+                                  backgroundColor: Colors.blue,
+                                  size: Size(90, 90),
+
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        )
                     ),
-                  )),
-                ],
-              )));
+                  );
+                },
+                  childCount: models.length
+                ),
+              )
+            ],
+          )
+      );
     }
   }
 
